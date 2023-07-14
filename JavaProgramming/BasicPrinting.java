f492086e2b6924068c49deb355cfd2ab62c69284
Sanjana S <sanjanasuresh291@gmail.com>
2023-07-14 20:49:08 +0530

    First program

---
+++ JavaProgramming/BasicPrinting.java
@@ -0,0 +1,9 @@
+package JavaProgramming;
+public class BasicPrinting
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("This is my 1st Program in Java :)");
+    }
+}
